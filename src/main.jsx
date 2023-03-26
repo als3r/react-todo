e15f1ae491e31ebf1bdd8a795225f7bcd59c293b
Alex <sergeyev.alex@gmail.com>
2023-03-25 23:50:35 -0400

    change main to use react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,52 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: 'tasklists',
+            element: <Tasklist />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: 'tasklist/:tasklistId',
+            element: <Tasklist />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: 'tasklist/:tasklistId/edit',
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: 'tasklist/:tasklistId/destroy',
+            action: destroyAction,
+            errorElement: <div>Oops! There was an error.</div>,
+          },
+        ],
+      },
+    ],
+  },
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />,
-  // <React.StrictMode>
-  // <App />
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
